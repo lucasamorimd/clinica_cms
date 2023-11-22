1af809b4c3c49b881b8f0c51b5fc654b0dd27c63
Lucas Amorim <lucasduarte.tecnologia@troupebrasil.com.br>
2023-11-21 21:07:51 -0300

    configurando docker

---
+++ app/Enums/Common/SituationEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums\Common;
+
+class SituationEnum
+{
+    const OPEN = 1;
+    const IN_PROGRESS = 2;
+    const FINISHED = 3;
+    const DELETED = 4;
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Funcionario;
+use App\Models\Medico;
+use App\Models\Servico;
+use App\Models\Unidade;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +18,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $unidades = Unidade::factory(2)->create();
+        foreach ($unidades as $unidade) {
+            $users =  User::factory(3)->has(Medico::factory()->for($unidade))->create();
+            if ($users::class == Collection::class) {
+                foreach ($users as $user) {
+                    Servico::factory(2)->hasAttached($user->medico->unidade)->hasAttached($user->medico)->create();
+                }
+            } else {
+                Servico::factory(2)->hasAttached($users->medico->unidade)->hasAttached($users->medico)->create();
+            }
+            User::factory(2)->has(Funcionario::factory()->for($unidade))->create();
+        }
     }
 }
